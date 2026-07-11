03c73ef884e96367b53f688b17d7256a85ed60ad
vs7554865-dot <vs7554865@gmail.com>
2026-07-11 11:35:37 +0530

    Dictionary-app structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Dictionary from "./components/Dictionary";
+
+const App = () => {
+  return (
+    <div>
+      <Dictionary/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Dictionary.jsx
@@ -0,0 +1,9 @@
+const Dictionary = () => {
+    return (
+        <div>
+            <h1>Dictionary App</h1>
+        </div>
+    );
+};
+
+export default Dictionary;
